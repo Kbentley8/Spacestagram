148c2fbaabc13f2c78f6e25a5e24b63c1dd8dbbe
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2022-01-19 19:09:58 -0500

    creating home page

--- spacestagram/src/components/App/App.js
+++ spacestagram/src/components/App/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {getPlanetPics} from '../../api-calls';
+import Planets from '../../Planets';
+import Loading from '../Loading';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      planetDetails: [],
+      error: null
+    }
+  }
+
+  componentDidMount = () => {
+    getPlanetPics()
+      .then(data => {
+        const updatedPictures = data.reduce((obj, hit) => {
+          let planetInfo = {
+            'date': hit.date,
+            'media': hit.media_type,
+            'title': hit.title,
+            'url': hit.url
+          }
+          obj.push(planetInfo);
+          return obj;
+        }, []);
+        this.setState({planetDetails: updatedPictures})
+      })
+      .catch(error => {
+        this.setState({error: 'Not able to get NASA Pictures try again some time later'})
+      })
+  }
+
+  render() {
+    return (
+      <main className='app'>
+      <h1 className='title'>Spacestagram</h1>
+      {!this.state.planetDetails.length && !this.state.error && <Loading />}
+      {this.state.error && <h2>{this.state.error}</h2>}
+      <Planets planetDetails={this.state.planetDetails} />
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ spacestagram/src/components/Loading/Loading.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../Loading/Loading.css';
+
+const Loading = () => {
+    return (
+            <section>
+                <div className= 'loading-styling'>
+                    <h1> Loading up NASA Astronomy 
+                        Pictures of the Day...
+                    </h1>
+                    <div className='loader'>
+                    <div className='loader-inner'>  
+                    </div>
+                    </div>
+                </div>
+            </section>
+
+
+
+
+    )
+
+
+}
+
+export default Loading;
